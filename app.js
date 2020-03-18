363e6f969ba6f7f54f226d7a0d45260ce0bf33f9
luzcry <tu-email@gmail.com>
2020-03-17 23:52:49 -0500

    transversing the DOM with siblings and parent nodes

--- app.js
+++ app.js
@@ -14,8 +14,33 @@ const wmf = document.querySelector("#book-list li:nth-child(2).name");
 let books = document.querySelectorAll("#book-list li.name");
 //console.log(books);
 
-Array.from(books).forEach(function (book) {
-    console.log(book);
-})
+Array.from(books).forEach(function(book) {
+  console.log(book);
+});
 
 const banner = document.querySelector("#page-banner");
+console.log("#page-banner node type is".banner.nodeType);
+console.log("#page-banner node type is".banner.nodeType);
+onsole.log("#page-banner has child".banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
+
+const bookList = document.querySelector("#book-list");
+//padres nodes
+console.log("the parent node is", bookList.parentNode);
+console.log("the parent node is", bookList.parentElement.parentElement);
+
+console.log(bookList.childNodes);
+console.log(bookList.children);
+//sibling nodes : same level
+
+const bookList = document.querySelector("#book-list");
+console.log("book-list next sibling is", bookList.nextSibling);
+console.log("book-list next element sibling is", bookList.nextElementSibling);
+
+console.log("book-list previous sibling is", bookList.previousSibling);
+console.log("book-list previous sibling is", bookList.previousElementSibling);
+
+bookList.previousElementSibling.querySelector("p").innerHTML +=
+  "<br /> too cool for anyone else!";
